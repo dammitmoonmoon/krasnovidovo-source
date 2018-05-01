f530094a71dfda69a68b57b0dce146ef526fa873
Yulia Modestova <jmodestova@yahoo.com>
2018-05-01 17:55:45 +0300

    language button, bilugual text

--- js/structure.js
+++ js/structure.js
@@ -4,3 +4,34 @@ const navLinks = document.querySelector('.nav-bar');
 navButton.addEventListener('click', () => {
     navLinks.classList.toggle('nav-bar-hidden');
 });
+
+let rusText = document.querySelectorAll('.rus');
+let engText = document.querySelectorAll('.eng');
+const toggleLang = document.querySelector('.language-button');
+
+
+if (toggleLang.value == "Rus") {
+    for (let index = 0; index < rusText.length; index++) {
+        rusText[index].classList.toggle('hidden-lang');        
+    }
+}
+if (toggleLang.value == "Eng") {
+    for (let index = 0; index < rusText.length; index++) {
+        engText[index].classList.toggle('hidden-lang');        
+    }
+}
+
+toggleLang.addEventListener('click', () => {
+    for (let index = 0; index < rusText.length; index++) {
+        rusText[index].classList.toggle('hidden-lang');        
+    }
+    for (let index = 0; index < engText.length; index++) {
+        engText[index].classList.toggle('hidden-lang');        
+    }
+    if (toggleLang.value == "Eng") {
+        toggleLang.value = "Rus";
+    }
+    else {
+        toggleLang.value = "Eng";
+    }
+});
