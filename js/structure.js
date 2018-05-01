9af16b62d79aacb7d56f870ebc9762a537cb26d2
Yulia Modestova <jmodestova@yahoo.com>
2018-05-01 16:05:52 +0300

    epic fail with page width

--- js/structure.js
+++ js/structure.js
@@ -2,18 +2,5 @@ const navButton = document.querySelector('.nav-button');
 const navLinks = document.querySelector('.nav-bar');
 
 navButton.addEventListener('click', () => {
-    if (navLinks.classList.contains('nav-bar-hidden')) {
-        navLinks.classList.remove('nav-bar-hidden');
-        setTimeout(function () {
-            navLinks.classList.remove('nav-bar-invis');
-            navLinks.classList.add('nav-bar-vis');
-        }, 200);
-    }
-    else {
-        navLinks.classList.remove('nav-bar-vis');
-        navLinks.classList.add('nav-bar-invis');
-        setTimeout(function () {
-            navLinks.classList.add('nav-bar-hidden');
-        }, 300);
-    }
+    navLinks.classList.toggle('nav-bar-hidden');
 });
